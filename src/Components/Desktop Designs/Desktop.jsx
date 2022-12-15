78f5c9d23040985dc7f7e67fa27a43acada81753
Dacron <favouradebimpe63@gmail.com>
2022-12-15 20:12:47 +0100

    last commit

--- src/Components/Desktop Designs/Desktop.jsx
+++ src/Components/Desktop Designs/Desktop.jsx
@@ -4,15 +4,17 @@ import './Desktop.css'
 import InterractiveDesktop from './Second-section desktop/InterractiveDesktop'
 import Creation from './DesktopCreations/Creation'
 import DesktopFooter from './Desktop Footer/DesktopFooter'
+import {Box} from '@mui/material'
+
 
 function Desktop() {
   return (
-    <>
+    <Box id='desktop-view'>
       <DesktopTop />
       <InterractiveDesktop />
        <Creation />
        <DesktopFooter />
-    </>
+    </Box>
   )
 }
 
